a6532a0f4fbfab6cf5c847ce4e75febd6469f2f6
Dima <akasa.akasa24@gmail.com>
2019-12-15 01:52:48 +0300

    fix commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
