19cf4ad27256c5c893b90163a17540322023b97f
Evgaizen <monkdval@mail.ru>
2019-12-20 19:45:48 +0300

    feat(cities): added seeders for cities

--- database/seeders/20191219173705-extra-options.js
+++ database/seeders/20191219173705-extra-options.js
@@ -10,63 +10,54 @@ module.exports = {
       "ExtraOptions",
       [
         {
-          id: 1,
           experience: "4 года",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 1
         },
         {
-          id: 2,
           experience: "5 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 2
         },
         {
-          id: 3,
           experience: "6 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 3
         },
         {
-          id: 4,
           experience: "7 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 4
         },
         {
-          id: 5,
           experience: "8 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 5
         },
         {
-          id: 6,
           experience: "9 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 6
         },
         {
-          id: 7,
           experience: "10 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 7
         },
         {
-          id: 8,
           experience: "11 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
           specializationId: 8
         },
         {
-          id: 9,
           experience: "12 лет",
           createdAt: new Date(),
           updatedAt: new Date(),
