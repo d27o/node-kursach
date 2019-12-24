3d88847cdca329806639a90b754715ea31c52e1c
Evgaizen <monkdval@mail.ru>
2019-12-25 00:35:52 +0300

    feat(view): changed page of registration

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -5,11 +5,11 @@ var exports = (module.exports = {});
 // };
 
 exports.login = function(req, res) {
-  res.render("login", {layout: 'loginMain'});
+  res.render("login", { layout: "loginMain" });
 };
 
 exports.home = function(req, res) {
-  res.render("home", {layout: "loginMain"});
+  res.render("home", { layout: "loginMain" });
 };
 
 exports.logout = function(req, res) {
@@ -19,99 +19,151 @@ exports.logout = function(req, res) {
 };
 
 exports.responsiblePeopleTable = function(req, res) {
-  require("../../database/models").ResponsiblePerson.findAll({
-    include: [{
-      model: require("../../database/models").Position
-    },
-    {
-      model: require("../../database/models").ExtraOptions,
-      include: [{
-        model: require("../../database/models").Specialization
-      }]
-    }
-  ]
-  }).then(function(person) {
-    res.render("responsiblePeopleTable", {responsiblePeople: person});
-  });
-}
+  require("../../database/models")
+    .ResponsiblePerson.findAll({
+      include: [
+        {
+          model: require("../../database/models").Position
+        },
+        {
+          model: require("../../database/models").ExtraOptions,
+          include: [
+            {
+              model: require("../../database/models").Specialization
+            }
+          ]
+        }
+      ]
+    })
+    .then(function(person) {
+      res.render("responsiblePeopleTable", { responsiblePeople: person });
+    });
+};
 
 exports.citiesTable = function(req, res) {
-  require("../../database/models").City.findAll().then(function(city) {
-    res.render("citiesTable", {cities: city});
-  });
-}
+  require("../../database/models")
+    .City.findAll()
+    .then(function(city) {
+      res.render("citiesTable", { cities: city });
+    });
+};
 
 exports.addressesTable = function(req, res) {
-  require("../../database/models").Address.findAll().then(function(addresses) {
-    res.render("addressesTable", {addresses: addresses});
-  });
-}
+  require("../../database/models")
+    .Address.findAll()
+    .then(function(addresses) {
+      res.render("addressesTable", { addresses: addresses });
+    });
+};
 
 exports.specializationsTable = function(req, res) {
-  require("../../database/models").Specialization.findAll().then(function(specializations) {
-    res.render("specializationsTable", {specializations: specializations});
-  });
-}
+  require("../../database/models")
+    .Specialization.findAll()
+    .then(function(specializations) {
+      res.render("specializationsTable", { specializations: specializations });
+    });
+};
 
 exports.extraOptionsTable = function(req, res) {
-  require("../../database/models").ExtraOptions.findAll().then(function(extraOptions) {
-    res.render("extraOptionsTable", {extraOptions: extraOptions});
-  });
-}
+  require("../../database/models")
+    .ExtraOptions.findAll()
+    .then(function(extraOptions) {
+      res.render("extraOptionsTable", { extraOptions: extraOptions });
+    });
+};
 
 exports.positionsTable = function(req, res) {
-  require("../../database/models").Position.findAll().then(function(positions) {
-    res.render("positionsTable", {positions: positions});
-  });
-}
+  require("../../database/models")
+    .Position.findAll()
+    .then(function(positions) {
+      res.render("positionsTable", { positions: positions });
+    });
+};
 
 exports.witnessesTable = function(req, res) {
-  require("../../database/models").Witness.findAll().then(function(witnesses) {
-    res.render("witnessesTable", {witnesses: witnesses});
-  });
-}
+  require("../../database/models")
+    .Witness.findAll()
+    .then(function(witnesses) {
+      res.render("witnessesTable", { witnesses: witnesses });
+    });
+};
 
 exports.typeOfDocumentsTable = function(req, res) {
-  require("../../database/models").TypeOfDocument.findAll().then(function(typeOfDocuments) {
-    res.render("typeOfDocumentsTable", {typeOfDocuments: typeOfDocuments});
-  });
-}
+  require("../../database/models")
+    .TypeOfDocument.findAll()
+    .then(function(typeOfDocuments) {
+      res.render("typeOfDocumentsTable", { typeOfDocuments: typeOfDocuments });
+    });
+};
 
 exports.placesTable = function(req, res) {
-  require("../../database/models").Place.findAll().then(function(places) {
-    res.render("placesTable", {places: places});
-  });
-}
+  require("../../database/models")
+    .Place.findAll()
+    .then(function(places) {
+      res.render("placesTable", { places: places });
+    });
+};
 
 exports.protocolsTable = function(req, res) {
-  require("../../database/models").Protocol.findAll().then(function(protocols) {
-    res.render("protocolsTable", {protocols: protocols});
-  });
-}
+  require("../../database/models")
+    .Protocol.findAll()
+    .then(function(protocols) {
+      res.render("protocolsTable", { protocols: protocols });
+    });
+};
 
 exports.documentsTable = function(req, res) {
-  require("../../database/models").Document.findAll().then(function(documents) {
-    res.render("documentsTable", {documents: documents});
-  });
-}
+  require("../../database/models")
+    .Document.findAll()
+    .then(function(documents) {
+      res.render("documentsTable", { documents: documents });
+    });
+};
 
 exports.typeOfMaterialEvidencesTable = function(req, res) {
-  require("../../database/models").TypeOfMaterialEvidence.findAll().then(function(typeOfMaterialEvidences) {
-    res.render("typeOfMaterialEvidencesTable", {typeOfMaterialEvidences: typeOfMaterialEvidences});
-  });
-}
+  require("../../database/models")
+    .TypeOfMaterialEvidence.findAll()
+    .then(function(typeOfMaterialEvidences) {
+      res.render("typeOfMaterialEvidencesTable", {
+        typeOfMaterialEvidences: typeOfMaterialEvidences
+      });
+    });
+};
 
 exports.materialEvidencesTable = function(req, res) {
-  require("../../database/models").MaterialEvidence.findAll().then(function(materialEvidences) {
-    res.render("materialEvidencesTable", {materialEvidences: materialEvidences});
-  });
-}
+  require("../../database/models")
+    .MaterialEvidence.findAll()
+    .then(function(materialEvidences) {
+      res.render("materialEvidencesTable", {
+        materialEvidences: materialEvidences
+      });
+    });
+};
 
 exports.registrationsTable = function(req, res) {
-  require("../../database/models").Registration.findAll().then(function(registrations) {
-    res.render("registrationsTable", {registrations: registrations});
-  });
-}
+  require("../../database/models")
+    .Registration.findAll({
+      include: [
+        {
+          model: require("../../database/models").ResponsiblePerson,
+          include: [{
+            model: require("../../database/models").Position
+          }]
+        },
+        {
+          model: require("../../database/models").MaterialEvidence,
+          include: [
+            {
+              model: require("../../database/models").TypeOfMaterialEvidence
+            }
+          ]
+        }
+      ]
+    })
+    .then(function(registrations) {
+      res.render("registrationsTable", { registrations: registrations });
+    });
+};
 
 exports.welcome = function(req, res) {
   res.render("welcome");
